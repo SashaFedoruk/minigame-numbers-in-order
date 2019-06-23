1642b55edec8129a681897316e552a64da14330b
Sasha Fedoruk <sasha1.fedoruk@gmail.com>
2019-06-23 16:39:32 +0200

    Finally commit

---
+++ littleGame/Form1.Designer.cs
@@ -0,0 +1,302 @@
+﻿namespace littleGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.Nums = new System.Windows.Forms.GroupBox();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.tBar = new System.Windows.Forms.TrackBar();
+            this.sTime = new System.Windows.Forms.TextBox();
+            this.sHit = new System.Windows.Forms.TextBox();
+            this.pBar = new System.Windows.Forms.ProgressBar();
+            this.Time = new System.Windows.Forms.NumericUpDown();
+            this.button14 = new System.Windows.Forms.Button();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.Nums.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.tBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Time)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(19, 19);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(141, 199);
+            this.listBox1.TabIndex = 0;
+            // 
+            // Nums
+            // 
+            this.Nums.Controls.Add(this.button8);
+            this.Nums.Controls.Add(this.button4);
+            this.Nums.Controls.Add(this.button7);
+            this.Nums.Controls.Add(this.button3);
+            this.Nums.Controls.Add(this.button10);
+            this.Nums.Controls.Add(this.button9);
+            this.Nums.Controls.Add(this.button6);
+            this.Nums.Controls.Add(this.button5);
+            this.Nums.Controls.Add(this.button2);
+            this.Nums.Controls.Add(this.button1);
+            this.Nums.Location = new System.Drawing.Point(181, 12);
+            this.Nums.Name = "Nums";
+            this.Nums.Size = new System.Drawing.Size(168, 205);
+            this.Nums.TabIndex = 1;
+            this.Nums.TabStop = false;
+            // 
+            // button8
+            // 
+            this.button8.Enabled = false;
+            this.button8.Location = new System.Drawing.Point(128, 83);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(34, 48);
+            this.button8.TabIndex = 0;
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Enabled = false;
+            this.button4.Location = new System.Drawing.Point(128, 16);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(34, 48);
+            this.button4.TabIndex = 0;
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Enabled = false;
+            this.button7.Location = new System.Drawing.Point(86, 83);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(34, 48);
+            this.button7.TabIndex = 0;
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(86, 16);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(34, 48);
+            this.button3.TabIndex = 0;
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            this.button10.Enabled = false;
+            this.button10.Location = new System.Drawing.Point(86, 151);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(34, 48);
+            this.button10.TabIndex = 0;
+            this.button10.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Enabled = false;
+            this.button9.Location = new System.Drawing.Point(46, 151);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(34, 48);
+            this.button9.TabIndex = 0;
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Enabled = false;
+            this.button6.Location = new System.Drawing.Point(46, 83);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(34, 48);
+            this.button6.TabIndex = 0;
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Enabled = false;
+            this.button5.Location = new System.Drawing.Point(6, 83);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(34, 48);
+            this.button5.TabIndex = 0;
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(46, 16);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(34, 48);
+            this.button2.TabIndex = 0;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(6, 16);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(34, 48);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(21, 224);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(139, 37);
+            this.button13.TabIndex = 2;
+            this.button13.Text = "Start";
+            this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.button13_Click);
+            // 
+            // tBar
+            // 
+            this.tBar.Enabled = false;
+            this.tBar.Location = new System.Drawing.Point(189, 236);
+            this.tBar.Name = "tBar";
+            this.tBar.Size = new System.Drawing.Size(233, 45);
+            this.tBar.TabIndex = 3;
+            // 
+            // sTime
+            // 
+            this.sTime.Enabled = false;
+            this.sTime.Location = new System.Drawing.Point(341, 293);
+            this.sTime.Name = "sTime";
+            this.sTime.Size = new System.Drawing.Size(81, 20);
+            this.sTime.TabIndex = 4;
+            // 
+            // sHit
+            // 
+            this.sHit.Enabled = false;
+            this.sHit.Location = new System.Drawing.Point(189, 293);
+            this.sHit.Name = "sHit";
+            this.sHit.Size = new System.Drawing.Size(104, 20);
+            this.sHit.TabIndex = 5;
+            // 
+            // pBar
+            // 
+            this.pBar.Location = new System.Drawing.Point(191, 273);
+            this.pBar.Name = "pBar";
+            this.pBar.Size = new System.Drawing.Size(230, 14);
+            this.pBar.TabIndex = 6;
+            // 
+            // Time
+            // 
+            this.Time.Increment = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.Time.Location = new System.Drawing.Point(27, 292);
+            this.Time.Maximum = new decimal(new int[] {
+            20000,
+            0,
+            0,
+            0});
+            this.Time.Minimum = new decimal(new int[] {
+            3000,
+            0,
+            0,
+            0});
+            this.Time.Name = "Time";
+            this.Time.Size = new System.Drawing.Size(65, 20);
+            this.Time.TabIndex = 7;
+            this.Time.Value = new decimal(new int[] {
+            3000,
+            0,
+            0,
+            0});
+            // 
+            // button14
+            // 
+            this.button14.Location = new System.Drawing.Point(24, 359);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(396, 28);
+            this.button14.TabIndex = 8;
+            this.button14.Text = "OK";
+            this.button14.UseVisualStyleBackColor = true;
+            // 
+            // timer
+            // 
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(434, 401);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.Time);
+            this.Controls.Add(this.pBar);
+            this.Controls.Add(this.sHit);
+            this.Controls.Add(this.sTime);
+            this.Controls.Add(this.tBar);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.Nums);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Nums.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.tBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Time)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.GroupBox Nums;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.TrackBar tBar;
+        private System.Windows.Forms.TextBox sTime;
+        private System.Windows.Forms.TextBox sHit;
+        private System.Windows.Forms.ProgressBar pBar;
+        private System.Windows.Forms.NumericUpDown Time;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ littleGame/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace littleGame
+{
+    public partial class Form1 : Form
+    {
+        private ArrayList _nums;
+        private Random rnd;
+        private int cClick;
+        private int TimerVal;
+
+        public Form1()
+        {
+            InitializeComponent();
+            _nums = new ArrayList();
+            rnd = new Random();
+            TimerVal = 0;
+            cClick = 0;
+            foreach (Control el in Nums.Controls)
+            {
+                el.Click += ValidClick;
+            }
+        }
+
+        private void ValidClick(object sender, EventArgs e)
+        {
+            cClick++;
+            int num = int.Parse(((Control)sender).Text);
+            if (_nums.IndexOf(num) == 0)
+            {
+                _nums.RemoveAt(0);
+                ((Control)sender).Enabled = false;
+                tBar.Value += 1;
+            }
+            if (_nums.Count == 0)
+            {
+                StopTimer();
+                double rez = cClick == 10 ? 10 : (100 / (cClick - 10));
+                sHit.Text = rez.ToString() + "%";
+                sTime.Text = TimerVal.ToString() + " msec";
+                MessageBox.Show("You Win!", "");
+            }
+            
+        }
+
+        private void InitButtons()
+        {
+            _nums.Clear();
+            int n;
+            
+            for (int i = 0; i < 10; i++)
+            {
+                do 
+                {
+                    n = rnd.Next(0, 100);
+                }while(_nums.IndexOf(n) != -1);
+                _nums.Add(n);
+            }
+
+            var buttons = Nums.Controls.OfType<Button>();
+            int idx = 0;
+            foreach (Button el in buttons)
+            {
+                el.Text = String.Format("{0}", _nums[idx++]);
+            }
+            _nums.Sort();
+        }
+
+
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            InitButtons();
+            timer.Interval = (int)Time.Value / 10;   
+           
+            foreach (Control el in Nums.Controls)
+            {
+                el.Enabled = true;
+            }
+            ClearData();
+            timer.Start();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            
+            int n = (int)Time.Value / 10;
+            TimerVal += n;
+            if (TimerVal <= (int)Time.Value)
+            {
+                  pBar.Value += 10;
+            }
+            else
+            {
+                StopTimer();
+                MessageBox.Show("You Lose!", "");
+            }        
+        }
+        private void ClearData(){
+            button13.Enabled = false;
+            tBar.Value = 0;
+            pBar.Value = 0;
+            cClick = 0;
+            sHit.Text = "";
+            sTime.Text = "";
+            TimerVal = 0;
+        }
+
+        private void StopTimer()
+        {
+            timer.Stop();
+            button13.Enabled = true;
+            foreach (Control el in Nums.Controls)
+            {
+                el.Enabled = false;
+            }
+        }
+    }
+}
